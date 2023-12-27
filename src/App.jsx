a999c128bf35eeb6457f0cb33fd2efcc17818cfc
christianlepio <rylepio79@gmail.com>
2023-12-27 16:38:39 +0800

    implement loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react'
 import AboutMe from './components/AboutMe'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
+import LoadingPage from './components/LoadingPage'
 import MyExperience from './components/MyExperience'
 import Projects from './components/Projects'
 import SayHi from './components/SayHi'
@@ -10,24 +12,35 @@ import ScrollButton from './components/ScrollButton'
 import { ThemeProvider } from './context/ThemeProvider'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3500);
+  }, [])
 
   return (
-    <>
       <ThemeProvider>
-        <Header />
-        <main className='mx-3'>
-          <div className="container-fluid">
-            <ScrollButton />
-            <Home />
-            <MyExperience />
-            <Projects />
-            <AboutMe />
-            <SayHi />
-          </div>
-        </main>
-        <Footer />
+        {isLoading ? (
+          <LoadingPage />
+        ) : (
+          <>
+            <Header />
+            <main className='mx-3'>
+              <div className="container-fluid">
+                <ScrollButton />
+                <Home />
+                <MyExperience />
+                <Projects />
+                <AboutMe />
+                <SayHi />
+              </div>
+            </main>
+            <Footer />
+          </>
+        )}
       </ThemeProvider>
-    </>
   )
 }
 
---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,30 @@
+import { useContext, useEffect } from 'react'
+import logoko from '/images/logoko.jpg'
+import ThemeContext from '../context/ThemeProvider'
+
+const LoadingPage = () => {
+    const {isDark} = useContext(ThemeContext)
+    useEffect(() => {
+        document.querySelector("html").setAttribute('data-bs-theme', (isDark ? 'dark' : 'light'))
+    }, [])
+
+    return (
+        <div className="loader" id="loader">
+            <div className="loading_3 d-flex justify-content-center mb-4">
+                <img 
+                    src={logoko} 
+                    width="77" 
+                    height="77" 
+                    class="logoko" 
+                    alt={logoko} 
+                />
+            </div>
+            <center>
+                <div className="loading_1 d-flex"></div>
+            </center>
+            <div className="loading_2 fw-bold">Loading.</div>
+        </div>
+    )
+}
+
+export default LoadingPage
