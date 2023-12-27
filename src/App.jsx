75eea30b88b69ab95e227c213904ec2d2ebfb5ba
christianlepio <rylepio79@gmail.com>
2023-12-27 11:26:35 +0800

    wrap components inside theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,26 @@ import Home from './components/Home'
 import MyExperience from './components/MyExperience'
 import Projects from './components/Projects'
 import SayHi from './components/SayHi'
+//context provider
+import ThemeProvider from './context/ThemeProvider'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <main className='mx-3'>
-        <div className="container-fluid">
-          <Home />
-          <MyExperience />
-          <Projects />
-          <AboutMe />
-          <SayHi />
-        </div>
-      </main>
-      <Footer />
+      <ThemeProvider>
+        <Header />
+        <main className='mx-3'>
+          <div className="container-fluid">
+            <Home />
+            <MyExperience />
+            <Projects />
+            <AboutMe />
+            <SayHi />
+          </div>
+        </main>
+        <Footer />
+      </ThemeProvider>
     </>
   )
 }
